5feef1d52465b63f92004270b110da61bb370b81
Suchendra Bharadwaj <suchendrabharadwaj@gmail.com>
2023-06-21 22:11:57 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+
+function app(){
+    return ( 
+    <div>
+   <Header />
+   <Note />
+   <Footer />
+   
+    </div>
+    );
+}
+
+export default App;
+
